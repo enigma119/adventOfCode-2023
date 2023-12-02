a263ce6497d2d0117f8e7b99a76af90b17fb3d84
Abass SARR <abasssarr0@gmail.com>
2023-12-02 00:50:01 +0000

    refacored second function

--- day1/index.js
+++ day1/index.js
@@ -25,52 +25,70 @@ var __importStar = (this && this.__importStar) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const fs = __importStar(require("fs"));
 const input = fs.readFileSync("input.txt", "utf8").split('\n');
-const stringDigits = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9 };
-const getFirstCalibration = () => {
-    let totalCalibration = 0;
-    input.map((line) => {
-        var _a;
-        let num = (_a = line.match(/\d+/g)) === null || _a === void 0 ? void 0 : _a.join('');
-        const calibration = parseInt(num.charAt(0).concat(num.charAt(num.length - 1)));
-        totalCalibration += calibration;
-    });
-    return totalCalibration;
+const stringDigits = {
+    'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+    'six': 6, 'seven': 7, 'eight': 8, 'nine': 9
 };
-const getSecondCalibration = () => {
+const getCalibration = (input, calibrationFunction) => {
+    return input.reduce((totalCalibration, line) => totalCalibration + calibrationFunction(line), 0);
+};
+const firstCalibrationFunction = (line) => {
+    var _a;
+    let num = (_a = line.match(/\d+/g)) === null || _a === void 0 ? void 0 : _a.join('');
+    return parseInt(num.charAt(0).concat(num.charAt(num.length - 1)));
+};
+const numbersMapping = {
+    'one': 'one1one',
+    'two': 'two2two',
+    'three': 'three3three',
+    'four': 'four4four',
+    'five': 'five5five',
+    'six': 'six6six',
+    'seven': 'seven7seven',
+    'eight': 'eight8eight',
+    'nine': 'nine9nine',
+};
+const secondCalibrationRefactoredFunction = () => {
     let totalCalibration = 0;
-    input.map((line) => {
-        let digits = [];
-        let combinedChar = '';
-        for (let i = 0; i < line.length; i++) {
-            const char = line.charAt(i);
-            if (Number.isNaN(parseInt(char))) {
-                combinedChar += char;
-                if (combinedChar.length >= 2) {
-                    const value = getResult(combinedChar);
-                    if (value) {
-                        digits.push(value);
-                        combinedChar = char;
-                    }
-                    ;
+    for (let line of input) {
+        for (const property in numbersMapping) {
+            line = line.replace(property, numbersMapping[property]);
+            console.log(line);
+        }
+        const numbers = line.split('').filter(char => !isNaN(parseInt(char)));
+        totalCalibration += parseInt(`${numbers[0] + numbers[numbers.length - 1]}`);
+    }
+};
+const secondCalibrationFunction = (line) => {
+    let digits = [];
+    let combinedChar = '';
+    for (let i = 0; i < line.length; i++) {
+        const char = line.charAt(i);
+        if (isNaN(parseInt(char))) {
+            combinedChar += char;
+            if (combinedChar.length >= 3) {
+                const value = getChartValue(combinedChar);
+                if (value) {
+                    digits.push(value);
+                    combinedChar = char;
                 }
-            }
-            else {
-                digits.push(parseInt(char));
-                combinedChar = '';
+                ;
             }
         }
-        const calibration = parseInt('' + digits[0] + digits[digits.length - 1]);
-        if (calibration)
-            totalCalibration += calibration;
-    });
-    return totalCalibration;
+        else {
+            digits.push(parseInt(char));
+            combinedChar = '';
+        }
+    }
+    return parseInt('' + digits[0] + digits[digits.length - 1]);
 };
-function getResult(digits) {
+function getChartValue(digits) {
     for (const property in stringDigits) {
         if (digits.includes(property)) {
             return stringDigits[property];
         }
     }
 }
-console.log(getFirstCalibration());
-console.log(getSecondCalibration());
+console.log(secondCalibrationRefactoredFunction());
+// console.log(getCalibration(input, firstCalibrationFunction));
+// console.log(getCalibration(input, secondCalibrationFunction));
--- day1/index.ts
+++ day1/index.ts
@@ -2,53 +2,78 @@ import * as fs from "fs";
 
 const input = fs.readFileSync("input.txt", "utf8").split('\n');
 
-const stringDigits: any = { 'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,'six': 6,'seven': 7,'eight': 8,'nine': 9 };
+const stringDigits: { [index: string]: number } = {
+    'one': 1, 'two': 2, 'three': 3, 'four': 4, 'five': 5,
+    'six': 6, 'seven': 7, 'eight': 8, 'nine': 9
+};
 
-const getFirstCalibration = () => {
-    let totalCalibration = 0;
-    input.map((line) => {
-        let num = line.match(/\d+/g)?.join('');
-        const calibration = parseInt(num!.charAt(0).concat(num!.charAt(num!.length - 1)));
-        totalCalibration += calibration;
-    })
-    return totalCalibration;
+const getCalibration = (input: string[], calibrationFunction: (line: string) => number) => {
+    return input.reduce((totalCalibration, line) => totalCalibration + calibrationFunction(line), 0);
 }
 
-const getSecondCalibration = () => {
+const firstCalibrationFunction = (line: string) => {
+    let num = line.match(/\d+/g)?.join('');
+    return parseInt(num!.charAt(0).concat(num!.charAt(num!.length - 1)));
+}
+
+
+const numbersMapping: { [index: string]: string } = {
+    'one': 'one1one',
+    'two': 'two2two',
+    'three': 'three3three',
+    'four': 'four4four',
+    'five': 'five5five',
+    'six': 'six6six',
+    'seven': 'seven7seven',
+    'eight': 'eight8eight',
+    'nine': 'nine9nine',
+}
+
+
+const secondCalibrationRefactoredFunction = () => {
     let totalCalibration = 0;
-    input.map((line) => {
-        let digits = []
-        let combinedChar = '';
-        for (let i = 0; i < line.length; i++) {
-            const char = line.charAt(i);
-            if (Number.isNaN(parseInt(char))) {
-                combinedChar += char;
-                if(combinedChar.length >= 2) {
-                    const value = getResult(combinedChar);
-                    if(value) {
-                        digits.push(value);
-                        combinedChar = char;
-                    };
-                }
-            } else {
-                digits.push(parseInt(char));
-                combinedChar = '';
-            }
+    for(let line of input) {
+        for (const property in numbersMapping) {
+            line = line.replaceAll(new RegExp(property, 'g'), numbersMapping[property]);
+            console.log(line);
         }
-        const calibration = parseInt('' + digits[0] + digits[digits!.length - 1]);
-        if(calibration) totalCalibration += calibration;
-    })
-    return totalCalibration;
-
+        const numbers = line.split('').filter(char => !isNaN(parseInt(char)));
+        totalCalibration += parseInt(`${numbers[0] + numbers[numbers.length - 1]}`);
+    }
 }
 
-function getResult(digits: any) {
-    for(const property in stringDigits) {
-        if(digits.includes(property)) {
+
+
+const secondCalibrationFunction = (line: string) => {
+    let digits = []
+    let combinedChar = '';
+    for (let i = 0; i < line.length; i++) {
+        const char = line.charAt(i);
+        if (isNaN(parseInt(char))) {
+            combinedChar += char;
+            if (combinedChar.length >= 3) {
+                const value = getChartValue(combinedChar);
+                if (value) {
+                    digits.push(value);
+                    combinedChar = char;
+                };
+            }
+        } else {
+            digits.push(parseInt(char));
+            combinedChar = '';
+        }
+    }
+    return parseInt('' + digits[0] + digits[digits!.length - 1]);
+}
+
+function getChartValue(digits: string) {
+    for (const property in stringDigits) {
+        if (digits.includes(property)) {
             return stringDigits[property]
         }
     }
 }
- 
-console.log(getFirstCalibration());
-console.log(getSecondCalibration());
+
+console.log(getCalibration(input, firstCalibrationFunction));
+console.log(getCalibration(input, secondCalibrationFunction));
+console.log(secondCalibrationRefactoredFunction());
